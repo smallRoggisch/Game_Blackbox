f9df788120964334782f3db5de907a66a2366b24
DelfaFi <56067574+DelfaFi@users.noreply.github.com>
2020-01-23 16:13:09 +0100

    Update StatisticCalculatorLocal.java
    Annotationen angefügt

--- Game-common/src/main/java/gameplay/common/StatisticCalculatorLocal.java
+++ Game-common/src/main/java/gameplay/common/StatisticCalculatorLocal.java
@@ -1,7 +1,7 @@
 package gameplay.common;
-
 import gameplay.treeobjects.StatisticScenarioPath;
 
+@Local
 public interface StatisticCalculatorLocal {
     public void updateCurrentGamepath(StatisticScenarioPath path);
     public boolean completeCurrentGamepath(StatisticScenarioPath path);

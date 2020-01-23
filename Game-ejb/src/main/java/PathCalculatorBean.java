720e5b4432b0d311055e8043adcfbdbc547e857c
DelfaFi <56067574+DelfaFi@users.noreply.github.com>
2020-01-23 16:17:48 +0100

    Create PathCalculatorBean.java
    PathCalculatorBean erstellt

---
+++ Game-ejb/src/main/java/PathCalculatorBean.java
@@ -0,0 +1,26 @@
+import blackbox.gameplay.common.PathCalculatorLocal;
+import gameplay.game.ScenarioCreator;
+import gameplay.treeobjects.Node;
+
+@Stateless
+public class PathCalculatorBean implements PathCalculatorLocal {
+    ScenarioCreator scenarioCreator;
+    
+    @Override
+    public Node getFollowingNode(long lastNode, long answerID) {
+    //TODO
+        return null;
+    }
+
+    @Override
+    public Node getNodeWithID(long lastNode) {
+    //TODO
+        return null;
+    }
+
+    @Override
+    public Node getStartNodeOfScenario(long scenarioID) {
+        scenarioCreator=new ScenarioCreator();
+        return scenarioCreator.getFirstNode(scenarioID);
+    }
+}
